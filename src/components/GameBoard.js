bc0d0352b8c5cd428555809650e5e2d20d712d28
Christopher Dennis <cdennis.aus@gmail.com>
2018-12-07 13:53:15 -0600

    Created initial Grid state and imported Grid component

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
+import Grid from './Grid';
 
 class GameBoard extends Component {
+  constructor() {
+    super();
+    this.rows = 30;
+    this.cols = 50;
+    this.speed = 150;
+    this.state = {
+      grid: Array(this.rows).fill(Array(this.cols).fill(false)),
+      generation: 0
+    }
+  }
+
+  boxClick = (row, col) => {
+    console.log(row, col);
+  }
+
   render() {
     return (
       <div id='game-board'>
-        <h1>GameBoard Component</h1>
+        <Grid
+          grid={this.state.grid}
+          rows={this.rows}
+          cols={this.cols}
+          boxClick={this.boxClick}
+        />
       </div>
     );
   }
