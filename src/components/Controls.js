6b97a6ef78919f2bf335b61e299b9383cd5fddef
Christopher Dennis <cdennis.aus@gmail.com>
2018-12-07 14:07:46 -0600

    Initial Controls component

---
+++ src/components/Controls.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Controls extends Component {
+  render() {
+    return (
+      <div id='controls'>
+        <h2>Controls Component</h2>
+      </div>
+    );
+  }
+}
+
+export default Controls;
