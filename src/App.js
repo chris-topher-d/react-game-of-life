92ffb821f068f3e00a91437dfb3adfe7babafb13
Christopher Dennis <cdennis.aus@gmail.com>
2018-12-06 21:49:46 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './app.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <h2>React Game of Life</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
