ad9ba48c3bf2492b9572d37796dd134f8c2cd98f
Christopher Dennis <cdennis.aus@gmail.com>
2018-12-06 22:21:04 -0600

    Created initial GameBoard component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import GameBoard from './components/GameBoard';
 import './app.css';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <h2>React Game of Life</h2>
+        <h2>Game of Life</h2>
+        <GameBoard />
       </div>
     );
   }
---
+++ src/components/GameBoard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class GameBoard extends Component {
+  render() {
+    return (
+      <div id='game-board'>
+        <h1>GameBoard Component</h1>
+      </div>
+    );
+  }
+}
+
+export default GameBoard;
