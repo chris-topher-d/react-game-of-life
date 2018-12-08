3c3e9676a07a34903d12a27177601553cd9204ce
Christopher Dennis <cdennis.aus@gmail.com>
2018-12-08 12:24:51 -0600

    Created seed function

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -14,10 +14,29 @@ class GameBoard extends Component {
     }
   }
 
+  componentDidMount() {
+    this.seed();
+  }
+
   // Changes state of cell to true or false when clicked
   boxClick = (row, col) => {
     let gridCopy = this.state.grid.map(array => array.slice());
     gridCopy[row][col] = !gridCopy[row][col];
+    this.setState({grid: gridCopy});
+  }
+
+  // Generate random live cells
+  seed = () => {
+    let gridCopy = this.state.grid.map(array => array.slice());
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        // Set approx 20% of random cells to true
+        if (Math.floor(Math.random() * 5) === 1) {
+          gridCopy[i][j] = true;
+        }
+      }
+    }
+
     this.setState({grid: gridCopy});
   }
 
